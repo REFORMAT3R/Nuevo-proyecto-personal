10d68d5276a697e3a20827b69f6dc23f8fefc47c
REFORMAT3R <dmamanihuayh@unsa.edu.pe>
2025-12-23 13:56:24 -0500

    Añadiendo jugadores

---
+++ Jugadores/Jugadores.js
@@ -0,0 +1,34 @@
+const content = document.querySelector(".players-grid");
+
+jugadores.forEach(jugador => {
+
+  // contenedor principal
+  const card = document.createElement("div");
+  card.classList.add("player-card");
+
+  // icono
+  const img = document.createElement("img");
+  img.src = `../PJ/${jugador.icono}`;
+  img.alt = jugador.personaje;
+
+  // top + nombre
+  const top = document.createElement("h3");
+  top.textContent = `#${jugador.top} ${jugador.nombre}`;
+
+  // personaje
+  const personaje = document.createElement("p");
+  personaje.textContent = `Personaje: ${jugador.personaje}`;
+
+  // nacionalidad
+  const nacionalidad = document.createElement("p");
+  nacionalidad.textContent = `País: ${jugador.nacionalidad}`;
+
+  // armar card
+  card.appendChild(img);
+  card.appendChild(top);
+  card.appendChild(personaje);
+  card.appendChild(nacionalidad);
+
+  // meter al content
+  content.appendChild(card);
+});
